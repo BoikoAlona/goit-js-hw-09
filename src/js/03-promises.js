cf54389ffa9ac054da6bec7cbe922bd082e72382
BoikoAlona <a.semikhova@gmail.com>
2023-11-14 17:05:08 +0200

    hw03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from "notiflix";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  for (let i = 0; i < form.amount.value; i++) {
+    let position = i + 1;
+    const promise = createPromise(i, Number(form.delay.value) + (form.step.value * i));
+    promise.then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+}
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    const obj = {
+      position,
+      delay,
+    };
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(obj);
+      } else {
+        reject(obj);
+      }
+    }, delay)
+  });;
+  return promise;
 }
